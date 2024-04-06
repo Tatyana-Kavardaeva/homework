ecf06537b0d210d2f877491d99d57203435355f4
Tatyana Kavardaeva <solarpoint@yandex.ru>
2024-04-06 09:53:57 +0300

    make_capitalize from str_func.py

--- str_func.py
+++ str_func.py
@@ -1,2 +1,8 @@
 def make_upper(words):
+    """Переводит строчные булвы в заглавные"""
     return words.upper()
+
+
+def make_capitalize(words):
+    """Озаглавливает первую букву первого слова"""
+    return words.capitalize()
