ea2d433566198210b61464d344cefbf58602acfa
Tatyana Kavardaeva <solarpoint@yandex.ru>
2024-04-06 09:16:12 +0300

    Add make_make_capitalize from str_func.py

--- str_func.py
+++ str_func.py
@@ -1,2 +1,8 @@
 def make_upper(words):
+    """Переводит всю строку в верхний регистр"""
     return words.upper()
+
+
+def make_capitalize(words):
+    """Озаглавливает первую букву первого слова"""
+    return words.capitalize()
